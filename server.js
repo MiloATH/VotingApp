c425b0ee18246a8c9d523a205212bc8431ac907f
Milo Hartsoe <milo.red.duck@gmail.com>
2016-12-19 18:17:42 +0000

    Add navbar and list view

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3500;
 
+
+
 app.listen(PORT,function(){
     console.log('Voting app listening on port',PORT);
 })
