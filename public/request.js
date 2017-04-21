a402e751dbd9f3eed0a929b2e2d62e3d6a5fc429
Milo Hartsoe <milo.red.duck@gmail.com>
2017-04-21 10:06:59 -0400

    Cleans profile view delete button UI

--- public/request.js
+++ public/request.js
@@ -9,7 +9,7 @@ function removePoll(id) {
                 $("#" + id).remove();
             } else {
                 //TODO: Better error handling ui
-                $("#msg").text('Error. Can\'t delete this poll.').addClass("alert-danger");
+                $("#msg").text("Error. Can't delete this poll.").addClass("alert-danger");
             }
         }
     });
@@ -27,15 +27,38 @@ function vote(answer) {
         data: data,
         success: function(msg) {
             if (msg.hasOwnProperty("success")) {
-                $("#msg").text(msg.success).addClass("alert-success");
+                $("#msg").text(msg.success).addClass("alert-success").fadeIn(0, function() {
+                    $(this).delay(3000).fadeOut(3000);
+                });
+                //TODO: Answer could be multiple words. Should use answer ids.
                 var voteCounter = $("#" + answer + " #votes");
                 voteCounter.text(+voteCounter.text() + 1);
-
+                //TODO Update data in piieData and call updateChart()
             } else {
                 //TODO: Better error handling ui
-                $("#msg").text('Error: ' + (msg.hasOwnProperty("error") ? msg.error : "") +
-                    ' Vote may not have been counted.').addClass("alert-danger");
+                $("#msg").text('Error: ' + (msg.hasOwnProperty("error") ? msg.error : "")).addClass("alert-danger").fadeIn(0, function() {
+                    $(this).delay(3000).fadeOut(3000);
+                });
             }
         }
     });
 };
+
+function updateChart() {
+    if (window.myPie !== undefined)
+        window.myPie.destroy();
+    var pie_ctx = document.getElementById("pie-chart-area").getContext("2d");
+    var myPie = new Chart(pie_ctx).Doughnut(pieData, {
+        responsive: true,
+        maintainAspectRatio: true,
+        legendTemplate: '<% for (var i=0; i < pieData.length; i++) { %>' +
+            '<div>' +
+            '<span id="legend-color" style=\"background-color:<%=pieData[i].color%>\"></span>' +
+            '<p class="legend-label"><% if (pieData [i].label) { %><%= pieData[i].label %><% } %></p>' +
+            '</div>' +
+            '<% } %>'
+    });
+
+    var legend = myPie.generateLegend();
+    $("#legend").html(legend);
+}
