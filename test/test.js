b656828b2433dff7a96441ae438f97a4b8cb4042
Milo Hartsoe <milo.red.duck@gmail.com>
2017-06-25 22:23:13 -0400

    Adds missing done callbacks for tests. Removes unnecessary login in simple poll test. Adds poll voting test.

--- test/test.js
+++ test/test.js
@@ -13,14 +13,16 @@ var colors = require('../app/utils/colors');
 
 describe('Unit Testing', function() {
     describe('colors.js', function() {
-        it('colors should be an array', function() {
+        it('colors should be an array', function(done) {
             assert.isTrue(Array.isArray(colors.colors));
+            done();
         });
-        it('colors should be an array of hex values', function() {
+        it('colors should be an array of hex values', function(done) {
             var isHex = /#[0-9A-Fa-f]{6}/;
             for (var i = 0; i < colors.colors.length; ++i) {
                 assert.match(colors.colors[i], isHex);
             }
+            done();
         });
     });
 
@@ -144,12 +146,14 @@ describe('Integration Testing', function() {
                 });
             });
 
-            it('should be successful', function() {
+            it('should be successful', function(done) {
                 browser.assert.success();
+                done();
             });
 
-            it('should see welcome page', function() {
+            it('should see welcome page', function(done) {
                 browser.assert.text('.container h2', 'Welcome, zombie');
+                done();
             });
         });
 
@@ -159,36 +163,62 @@ describe('Integration Testing', function() {
             var options = ['Yes', 'No'];
 
             before(function(done) {
-                browser.visit('/login', function() {
+                browser.visit('/make', function() {
                     browser
-                        .fill('username', 'zombie')
-                        .fill('password', 'eat-the-living')
-                        .pressButton('Submit', function() {
-                            browser.visit('/make', function() {
-                                browser
-                                    .fill('question', question)
-                                    .fill('options[answer1]', options[0])
-                                    .fill('options[answer2]', options[1])
-                                    .pressButton('Submit', (err) => {
-                                        //Throws err becuase zombie has issues with the canvas for the chart
-                                        //Wrapping done callback in a callback avoids err being thrown
-                                        done();
-                                    });
-                            });
+                        .fill('question', question)
+                        .fill('options[answer1]', options[0])
+                        .fill('options[answer2]', options[1])
+                        .pressButton('Submit', function(err) {
+                            //Throws err becuase zombie has issues with the canvas for the chart
+                            //Wrapping done callback in a callback avoids err being thrown
+                            done();
                         });
                 });
             });
 
-            it('should be successful', function() {
+            it('should be successful', function(done) {
                 browser.assert.success();
+                done();
             });
 
-            it('should display question', function() {
+            it('should display the question', function(done) {
                 browser.assert.text('h2.title', question);
+                done();
             });
 
-            it('should display options', function() {
+            it('should display the options', function(done) {
                 browser.assert.text('span#answer', options.join(''));
+                done();
+            });
+
+            it('should display the options vote count as 0', function(done) {
+                browser.assert.text('span#votes', '0'.repeat(options.length));
+                done();
+            });
+
+        });
+
+        describe('vote on poll', function() {
+
+            var question = 'Can I make a poll?';
+            var options = ['Yes', 'No'];
+
+            before(function(done) {
+                browser.pressButton('Yes 0', function(err) {
+                    //Throws err becuase zombie has issues with the canvas for the chart
+                    //Wrapping done callback in a callback avoids err being thrown
+                    done();
+                });
+            });
+
+            it('should tell the user the vote counted', function(done) {
+                browser.assert.text('.alert.fixed-msg', 'Vote counted.');
+                done();
+            });
+
+            it('should increase the number of votes by 1', function(done) {
+                browser.assert.text('span#votes', '1' + '0'.repeat(options.length - 1));
+                done();
             });
 
         });
